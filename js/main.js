e1fe667b7dfa540641b9582ab056ea1a474e0a19
Ivan Blinkov <ivan@blinkov.ru>
2016-02-27 19:08:06 +0300

    add images to slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+function slider(running) {
+
+}
 $(document).ready(function() {
-    $('#slider').tinycarousel();
+    $('#logo').on('click', function (e) {
+        e.preventDefault();
+
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+
+    });
+    var slider = $('#slider').tinycarousel({
+        interval: true
+    }).data("plugin_tinycarousel");
+
+    var popover = $('[data-toggle="popover"]');
+    popover.popover();
+    popover.on('show.bs.popover', function () {
+        slider.stop();
+    });
+    popover.on('hidden.bs.popover', function () {
+        slider.start();
+    });
 });
